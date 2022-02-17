ce46ce2d0ff6e022bf88dff8a90a6468320fae9b
ProfMoo <obshane96@gmail.com>
2022-02-16 21:11:05 -0500

    changed some config, added README notes

--- beets/entrypoint.sh
+++ beets/entrypoint.sh
@@ -9,6 +9,13 @@ do
     echo '[ENTRYPOINT SCRIPT] Running beets import'
     beet import /downloads
 
+    echo '[ENTRYPOINT SCRIPT] Running beets move in case there is new configuration that needs to be backported
+    , or there are duplicates recently added that need to be disambiguated.'
+    beet move
+
+    echo '[ENTRYPOINT SCRIPT] Running beets update in case there are any metadata updates'
+    beet update
+
     echo '[ENTRYPOINT SCRIPT] Sleeping...'
     sleep 100
 done
