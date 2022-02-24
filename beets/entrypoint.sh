17593db53482653f5261d48cbd497f2528704847
ProfMoo <obshane96@gmail.com>
2022-02-23 22:00:11 -0500

    some more configuration

--- beets/entrypoint.sh
+++ beets/entrypoint.sh
@@ -5,18 +5,18 @@ beet --help
 
 echo '[ENTRYPOINT SCRIPT] Continuously importing music files'
 
-sleep 10000
-# while true
-# do
-#     echo '[ENTRYPOINT SCRIPT] Running beets import'
-#     beet import /downloads
+while true
+do
+    echo '[ENTRYPOINT SCRIPT] Running beets import'
+    beet import /downloads
 
-#     echo '[ENTRYPOINT SCRIPT] Running beets move in case there is new configuration that needs to be backported, or there are duplicates recently added that need to be disambiguated.'
-#     beet move
+    echo '[ENTRYPOINT SCRIPT] Running beets move in case there is new configuration that needs to be backported, or there are duplicates recently added that need to be disambiguated.'
+    beet move
 
-#     echo '[ENTRYPOINT SCRIPT] Running beets update in case there are any metadata updates'
-#     beet update
+    # Commenting this out until I understand why beet update continually updates the same info
+    # echo '[ENTRYPOINT SCRIPT] Running beets update in case there are any metadata updates'
+    # beet update
 
-#     echo '[ENTRYPOINT SCRIPT] Sleeping for 100 seconds...'
-#     sleep 100
-# done
+    echo '[ENTRYPOINT SCRIPT] Sleeping for 100 seconds...'
+    sleep 100
+done
