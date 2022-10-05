72ae7fc4fd3561a26cba43efe5d52aeaab5e9144
ProfMoo <obshane96@gmail.com>
2022-10-04 21:35:12 -0400

    Removed 'yq' dependency in favor of native bash

---
+++ config/nas.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# I use this file when running the containers on the NAS
+
+TRACKER_DIR="/volume2/music/torrent-files"
+DOWNLOAD_DIR="/volume2/music/pre-process"
+MUSIC_LIBRARY_DIR="/volume2/music/post-process"
--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # Getting the location of the config file
-CONFIG_FILE=$1
+RUN_TYPE=$1
 
 echo "Starting up the docker container for the home setup..."
 
-TRACKER_DIR=$(cat $CONFIG_FILE | yq eval '.TRACKER_DIR' -)
-echo "Tracker directory: $TRACKER_DIR"
+if [ "$RUN_TYPE" = "local" ]; then
+    echo "Starting up with the 'local' configuration"
+    source config/local.sh
+elif [ "$RUN_TYPE" = "nas" ]; then
+    echo "Starting up with the 'nas' configuration"
+    source config/nas.sh
+else
+    echo "Run type '$RUN_TYPE' not supported. Exiting..."
+fi
 
-DOWNLOAD_DIR=$(cat $CONFIG_FILE | yq eval '.DOWNLOAD_DIR' -)
-echo "Download directory: $DOWNLOAD_DIR"
-
-MUSIC_LIBRARY_DIR=$(cat $CONFIG_FILE | yq eval '.MUSIC_LIBRARY_DIR' -)
-echo "Music library directory: $MUSIC_LIBRARY_DIR"
-
-if TRACKER_DIR=$TRACKER_DIR DOWNLOAD_DIR=$DOWNLOAD_DIR MUSIC_LIBRARY_DIR=$MUSIC_LIBRARY_DIR docker compose build --parallel ; then
-    TRACKER_DIR=$TRACKER_DIR DOWNLOAD_DIR=$DOWNLOAD_DIR MUSIC_LIBRARY_DIR=$MUSIC_LIBRARY_DIR docker compose up --remove-orphans
+if docker compose build --parallel ; then
+    docker compose up --remove-orphans
 else
     echo "Docker compose build failed"
 fi
