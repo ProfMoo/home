0be9400813cfd3640af04010c9f22177d397d0a6
ProfMoo <obshane96@gmail.com>
2022-10-04 21:36:51 -0400

    Added missing 'export' keyword

--- config/nas.sh
+++ config/nas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # I use this file when running the containers on the NAS
 
-TRACKER_DIR="/volume2/music/torrent-files"
-DOWNLOAD_DIR="/volume2/music/pre-process"
-MUSIC_LIBRARY_DIR="/volume2/music/post-process"
+export TRACKER_DIR="/volume2/music/torrent-files"
+export DOWNLOAD_DIR="/volume2/music/pre-process"
+export MUSIC_LIBRARY_DIR="/volume2/music/post-process"
