153fdc9399a417e56c7608b6c7aac8b00d3a9c5f
ProfMoo <obshane96@gmail.com>
2023-03-02 21:40:39 -0500

    Add factorio server
    Also cleaning up some READMEs and docker-compose files was I had the chance

--- factorio/docker_run.sh
+++ factorio/docker_run.sh
@@ -3,21 +3,21 @@
 # Determining what type of startup we will need
 RUN_TYPE=$1
 
-echo "Starting up the docker container for the home setup..."
+echo "Starting up the factorio server..."
 
 if [ "$RUN_TYPE" = "local" ]; then
     echo "Starting up with the 'local' configuration"
-    source config/local.sh
+    export FACTORIO_DIR="/mnt/z/factorio"
 elif [ "$RUN_TYPE" = "nas" ]; then
     echo "Starting up with the 'nas' configuration"
-    source config/nas.sh
+    export FACTORIO_DIR="/volume2/docker/factorio"
     RUN_ARGS="--detach"
 else
     echo "Run type '$RUN_TYPE' not supported. Supported types are 'local' and 'nas'. Exiting..."
 fi
 
-if docker-compose build; then
-    docker-compose up --remove-orphans ${RUN_ARGS}
+if docker compose build --parallel; then
+    docker compose --env-file .env up --remove-orphans ${RUN_ARGS}
 else
     echo "Docker compose build failed"
 fi
