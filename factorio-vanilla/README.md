4d333285e0160d1b7e8c227ba6136edbd83fa58b
ProfMoo <obshane96@gmail.com>
2023-04-08 10:52:08 -0400

    Add new factorio server

---
+++ factorio-vanilla/README.md
@@ -0,0 +1,9 @@
+# Factorio Server
+
+This is the first shared server the OB Kiddos made. This is the one with the large basement and the oversized Iron farm.
+
+## Quick Start
+
+```bash
+./docker_run.sh local
+```
---
+++ factorio-vanilla/docker_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Determining what type of startup we will need
+RUN_TYPE=$1
+
+echo "Starting up the factorio server..."
+
+if [ "$RUN_TYPE" = "local" ]; then
+    echo "Starting up with the 'local' configuration"
+    export FACTORIO_DIR="/mnt/z"
+elif [ "$RUN_TYPE" = "nas" ]; then
+    echo "Starting up with the 'nas' configuration"
+    export FACTORIO_DIR="/volume2/docker/factorio"
+    RUN_ARGS="--detach"
+else
+    echo "Run type '$RUN_TYPE' not supported. Supported types are 'local' and 'nas'. Exiting..."
+fi
+
+if docker compose build --parallel; then
+    docker compose --env-file .env up --remove-orphans ${RUN_ARGS}
+else
+    echo "Docker compose build failed"
+fi
