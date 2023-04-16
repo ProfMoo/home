6bcc7c904e6ac0833d8909ad10bc17e90d25f18a
ProfMoo <obshane96@gmail.com>
2023-04-15 21:11:31 -0400

    Working on fixing the qBittorrent mounting

--- config/local.sh
+++ config/local.sh
@@ -2,5 +2,7 @@
 # I use this file when running this directory from my development environment (i.e. desktop)
 
 export TRACKER_DIR="/mnt/x/torrent-files"
+export ACTIVE_TORRENTS="/mnt/x/active-torrents"
 export DOWNLOAD_DIR="/mnt/x/pre-process"
 export MUSIC_LIBRARY_DIR="/mnt/x/post-process"
+export IN_PROGRESS_TORRENTS="/mnt/x/torrents-in-progress"
