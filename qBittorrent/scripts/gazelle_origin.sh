cf92c653b01ec4e1b52dbb2291ccbd63f28067d7
ProfMoo <obshane96@gmail.com>
2023-10-05 19:26:07 -0400

    Added Orpheus support to gazelle-origin

--- qBittorrent/scripts/gazelle_origin.sh
+++ qBittorrent/scripts/gazelle_origin.sh
@@ -7,22 +7,40 @@
 # For more info about these variables, refer here: https://github.com/x1ppy/gazelle-origin#installation
 
 # Converting bash script inputs into readable env vars
+
 ROOT_PATH=$1
 echo "ROOT_PATH: $ROOT_PATH"
+
 INFO_HASH=$2
 echo "INFO_HASH: $INFO_HASH"
+
 CURRENT_TRACKER_OR_CAT=$3
-echo "CURRENT_TRACKER_OR_CAT: $CURRENT_TRACKER_OR_CAT"
+# NOTE: Not printing this because it could contain secret announce URL
+
 ROOT_BASE=`basename "$1"`
 echo "ROOT_BASE: $ROOT_BASE"
 
 if [[ $CURRENT_TRACKER_OR_CAT != "" ]]; then
   if [[ "$CURRENT_TRACKER_OR_CAT" == *"flacsfor.me"* || "$CURRENT_TRACKER_OR_CAT" == *"RED"* ]]; then
-    echo "[gazelle_origin]: Grabbing torrent info for $ROOT_BASE from RED."
-    gazelle-origin -t red -o "$ROOT_PATH"/origin.yaml --api-key $RED_API_KEY $INFO_HASH
-    echo "[gazelle origin]: Finished grabbing torrent info for '$ROOT_BASE' from RED."
+    echo "[gazelle_origin]: Grabbing torrent info for $ROOT_BASE from RED..."
+
+    if ! gazelle-origin -t red -o "$ROOT_PATH"/origin.yaml --api-key $RED_API_KEY $INFO_HASH; then
+        echo "[gazelle_origin]: Grabbing torrent info for $ROOT_BASE from RED failed."
+    else
+        echo "[gazelle_origin]: Grabbing torrent info for $ROOT_BASE from RED succeeded."
+    fi
+
+  elif [[ "$CURRENT_TRACKER_OR_CAT" == *"opsfet.ch"* || "$CURRENT_TRACKER_OR_CAT" == *"ORP"* ]]; then
+    echo "[gazelle_origin]: Grabbing torrent info for $ROOT_BASE from OPS..."
+
+    if ! gazelle-origin -t ops -o "$ROOT_PATH"/origin.yaml --api-key $OPS_SESSION_COOKIE $INFO_HASH; then
+        echo "[gazelle_origin]: Grabbing torrent info for $ROOT_BASE from OPS failed."
+    else
+        echo "[gazelle_origin]: Grabbing torrent info for $ROOT_BASE from OPS succeeded."
+    fi
+    
   else
-    echo "[gazelle_origin]: No match on '$ROOT_BASE' with category/tracker of '$CURRENT_TRACKER_OR_CAT'. Load again in a matching category, or redownload live, to reprocess."
+    echo "[gazelle_origin]: No match on download '$ROOT_BASE' with category/tracker of '$CURRENT_TRACKER_OR_CAT'. Load again in a matching category, or redownload live, to reprocess."
   fi
 else
   echo "No matches on '$ROOT_BASE' (category and tracker are both blank). Load again in a matching category, or redownload live, to reprocess."
