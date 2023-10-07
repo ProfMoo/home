68404a10ebb92349d9271a3608e61f0680214181
ProfMoo <obshane96@gmail.com>
2023-10-06 21:13:06 -0400

    Cleaning up betanin addition

--- qBittorrent/scripts/betanin.sh
+++ qBittorrent/scripts/betanin.sh
@@ -7,7 +7,8 @@ echo "[betanin]: Adding new torrent '$PATH' to betanin queue..."
 
 echo "[betanin]: Running curl against betanin..."
 
-if ! curl --request POST --data-urlencode "both=$PATH" --header "X-API-Key: f3d5a5a95fad096470ca411eb41161fb" http://host.docker.internal:9393/api/torrents; then
+# NOTE: For some reason I need to specify the full path for curl to work here
+if ! /usr/bin/curl --request POST --data-urlencode "both=$PATH" --header "X-API-Key: f3d5a5a95fad096470ca411eb41161fb" http://host.docker.internal:9393/api/torrents; then
     echo "[betanin]: Adding new torrent '$PATH' to betanin queue failed."
 else
     echo "[betanin]: Adding new torrent '$PATH' to betanin queue succeeded."
