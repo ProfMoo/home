8f3cc7c48162727436d403f271b75322608f49fc
ProfMoo <obshane96@gmail.com>
2023-10-06 21:24:20 -0400

    Various small bug fixes related to new beets container

--- beets/scripts/delete-from-disk.sh
+++ beets/scripts/delete-from-disk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Script to assist in deleting a release entirely from disk (rather than just from the beets DB).
 # The script will ask you for a confirmation before deleting from disk permanently.
