2713060e42a700f06c02cdcda9503113d547f665
ProfMoo <obshane96@gmail.com>
2023-10-06 21:13:06 -0400

    Cleaning up betanin addition

--- qBittorrent/scripts/post_download.sh
+++ qBittorrent/scripts/post_download.sh
@@ -3,9 +3,46 @@
 # NOTE: This gets the directory of this bash script
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Reassigning all the inputs from qBittorrent to a human-readable name
+TORRENT_NAME=$1
+echo "TORRENT_NAME: $TORRENT_NAME"
+
+CATEGORY=$2
+echo "CATEGORY: $CATEGORY"
+
+TAGS=$3
+echo "TAGS: $TAGS"
+
+CONTENT_PATH=$4
+echo "CONTENT_PATH: $CONTENT_PATH"
+
+ROOT_PATH=$5
+echo "ROOT_PATH: $ROOT_PATH"
+
+SAVE_PATH=$6
+echo "SAVE_PATH: $SAVE_PATH"
+
+NUMBER_OF_FILES=$7
+echo "NUMBER_OF_FILES: $NUMBER_OF_FILES"
+
+TORRENT_SIZE=$8
+echo "TORRENT_SIZE: $TORRENT_SIZE"
+
+CURRENT_TRACKER=$9
+echo "CURRENT_TRACKER: $CURRENT_TRACKER"
+
+INFO_HASH_V1="${10}"
+echo "INFO_HASH_V1: $INFO_HASH_V1"
+
+INFO_HASH_V2="${11}"
+echo "INFO_HASH_V2: $INFO_HASH_V2"
+
+TORRENT_ID="${12}"
+echo "TORRENT_ID: $TORRENT_ID"
+
 echo "[post_download]: Running post-download scripts..."
 
-source $DIR/gazelle_origin.sh "$5" "$10" "$9" "$2"
-source $DIR/betanin.sh "$5"
+source $DIR/gazelle_origin.sh "${ROOT_PATH}" "${INFO_HASH_V1}" "${CURRENT_TRACKER}"
+source $DIR/betanin.sh "${ROOT_PATH}"
 
 echo "[post_download]: Completed post-download scripts."
