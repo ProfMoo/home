8a01f739e66e055169e8770ac833f08570326d74
Shane Moo <obshane96@gmail.com>
2023-10-22 22:35:29 -0400

    Big changes to directory structure, still carrying that through the whole stack

--- config/nas.sh
+++ config/nas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # I use this file when running the containers on the NAS
 
-export TRACKER_DIR="/volume2/music/torrent-files"
+export TORRENT_DIR="/volume2/music/torrent-files"
 export DOWNLOAD_DIR="/volume2/music/pre-process"
 export MUSIC_LIBRARY_DIR="/volume2/music/post-process"
 export ACTIVE_TORRENTS="/volume2/music/active-torrents"
