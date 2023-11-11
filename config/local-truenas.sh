6bb0e745d7ad15da6f179e5d85f9d9af7b6a638a
Shane Moo <obshane96@gmail.com>
2023-11-11 15:04:56 -0500

    Add configuration option for TrueNAS NAS

---
+++ config/local-truenas.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# I use this file when running this directory from my development environment (i.e. desktop)
+
+# Data
+export DATA_DIR="/mnt/t"
+
+# Downloads
+export DOWNLOAD_DIR="/mnt/t/downloads"
+export MUSIC_DOWNLOAD_DIR="/mnt/t/downloads/music"
+export MOVIES_DOWNLOAD_DIR="/mnt/t/downloads/movies"
+
+# Media
+export LIBRARY_DIR="/mnt/t/media"
+export MUSIC_LIBRARY_DIR="/mnt/t/media/music"
+export MOVIE_LIBRARY_DIR="/mnt/t/media/movies"
+
+# Torrents
+export INPUT_TORRENT_DIR="/mnt/t/torrents/input"
+export INPUT_TORRENT_MUSIC_DIR="/mnt/t/torrents/input/music"
+export INPUT_TORRENT_MOVIES_DIR="/mnt/t/torrents/input/movies"
+export ACTIVE_TORRENTS="/mnt/t/torrents/active-torrents"
+export IN_PROGRESS_TORRENTS="/mnt/t/torrents/torrents-in-progress"
+
+# Container volumes
+export BEETS_DIR="/mnt/v/beets"
+export BETANIN_DIR="/mnt/v/betanin"
+export PROWLARR_DIR="/mnt/v/prowlarr"
+export JELLYFIN_DIR="/mnt/v/jellyfin"
+export CROSS_SEED_DIR="/mnt/v/cross-seed"
+export RADARR_DIR="/mnt/v/radarr"
+export SONARR_DIR="/mnt/v/sonarr"
