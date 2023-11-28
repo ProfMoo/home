becfa887180ddb914db58a6b3cbe5614b3c282fd
Shane Moo <obshane96@gmail.com>
2023-11-28 11:01:48 -0500

    Moving into my own folder since I've moving away from TKS significantly

---
+++ infrastructure/talos/provider.tf
@@ -0,0 +1,36 @@
+provider "talos" {}
+
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.38.1"
+    }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "0.3.4"
+    }
+    macaddress = {
+      source = "ivoronin/macaddress"
+      version = "0.3.0"
+    } 
+  }
+
+  backend "s3" {
+    bucket = "sobrien-home"
+    key    = "infrastructure"
+    region = "us-east-1"
+    # To authenticate to access the state (not the actual terraform commands)
+    shared_credentials_file = "./aws-credentials"
+  }
+}
+
+provider "proxmox" {
+  endpoint = "https://192.168.1.64:8006/api2/json"
+  api_token = "root@pam!tjs-test-2=fd03ad68-f6fd-4610-8dc2-4b6af00e7407"
+  insecure  = true
+  ssh {
+    agent    = true
+    username = "root"
+  }
+}
