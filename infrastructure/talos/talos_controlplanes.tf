c37ecf40c23a0e20f4c63f67477ec4fe797ac69a
Shane Moo <obshane96@gmail.com>
2023-11-28 11:01:48 -0500

    Moving into my own folder since I've moving away from TKS significantly

---
+++ infrastructure/talos/talos_controlplanes.tf
@@ -0,0 +1,103 @@
+resource "proxmox_virtual_environment_vm" "controlplane" {
+  depends_on = [
+    macaddress.control_plane_mac_addresses
+  ]
+
+
+  count = var.controlplane_num
+  vm_id = "${var.controlplane_vmid_prefix}${count.index + 1}"
+
+  pool_id   = proxmox_virtual_environment_pool.proxmox_resource_pool.id
+  node_name = var.controlplane_node_name
+
+  name        = "${var.controlplane_hostname_prefix}-${count.index + 1}"
+  description = "${var.controlplane_hostname_prefix}: ${count.index + 1}"
+  tags        = var.controlplane_tags
+
+  cpu {
+    cores = var.controlplane_cpu_cores
+    type  = "host"
+  }
+
+  memory {
+    dedicated = var.controlplane_memory
+  }
+
+  network_device {
+    bridge      = var.controlplane_network_device
+    mac_address = macaddress.control_plane_mac_addresses[count.index].address
+    vlan_id     = var.controlplane_vlan_id
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "${var.controlplane_ip_prefix}${count.index + 1}/24"
+        gateway = var.gateway_ip
+      }
+    }
+  }
+
+  disk {
+    datastore_id = var.controlplane_datastore
+    file_id      = proxmox_virtual_environment_file.talos_image.id
+    file_format  = "raw"
+    interface    = "scsi0"
+    size         = var.controlplane_disk_size
+  }
+
+  agent {
+    enabled = true
+    # If left to default, Terraform will be held for 15 minutes waiting for the agent to start
+    # before it is even installed. Prevent this by effectively immediatly timing out.
+    timeout = "1s"
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  # Remove the node from Kubernetes on destroy
+  provisioner "local-exec" {
+    when    = destroy
+    command = "./bin/manage_nodes remove ${self.name}"
+  }
+}
+
+resource "macaddress" "control_plane_mac_addresses" {
+  count = var.controlplane_num
+}
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = var.kubernetes_cluster_name
+  cluster_endpoint = "https://${var.talos_virtual_ip}:6443"
+
+  machine_type    = "controlplane"
+  machine_secrets = talos_machine_secrets.this.machine_secrets
+
+  talos_version      = var.talos_version
+  kubernetes_version = var.kubernetes_version
+}
+
+resource "talos_machine_configuration_apply" "controlplane" {
+  depends_on = [
+    proxmox_virtual_environment_vm.controlplane
+  ]
+
+  count    = var.controlplane_num
+  node     = local.controlplane_nodes[count.index]
+  endpoint = local.controlplane_nodes[count.index]
+
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+
+  config_patches = [
+    templatefile("configs/global.yml", {
+      qemu_guest_agent_version = var.qemu_guest_agent_version
+    }),
+    templatefile("configs/controlplane.yml", {
+      talos_virtual_ip = var.talos_virtual_ip
+    }),
+    var.talos_disable_flannel ? templatefile("configs/disable_flannel.yml", {}) : null
+  ]
+}
---
+++ infrastructure/talos/vars/stable.tfvars
@@ -0,0 +1,46 @@
+# Proxmox #######################
+proxmox_hostname                = "earth.sol.milkyway"
+proxmox_ssh_key_path            = "~/.ssh/sol.milkyway"
+proxmox_resource_pool           = "Kubernetes-Stable"
+
+
+# Talos #########################
+talos_image_node_name           = "earth"
+talos_virtual_ip                = "192.168.1.20"
+talos_disable_flannel           = true
+
+
+# Kubernetes ####################
+kubernetes_cluster_name         = "stable"
+
+
+# Controlplanes #################
+controlplane_vmid_prefix        = "402"               # 4011-4019
+controlplane_node_name          = "mario"
+
+controlplane_hostname_prefix    = "k8s-cp"
+controlplane_ip_prefix          = "192.168.1.1"      # 11-19
+controlplane_mac_address_prefix = "00:00:00:00:00:1"  # 00:11 - 00:19
+controlplane_vlan_id            = "40"
+
+controlplane_tags               = [
+  "app-kubernetes",
+  "clusterid-stable",
+  "type-controlplane"
+]
+
+
+# Worker Nodes ##################
+workernode_vmid_prefix          = "403"               # 4021-4029
+workernode_node_name            = "toad"
+
+workernode_hostname_prefix      = "k8s-node"
+workernode_ip_prefix            = "192.168.1.2"      # 21-29
+workernode_mac_address_prefix   = "00:00:00:00:00:2"  # 00:21 - 00:29
+workernode_vlan_id              = "40"
+
+workernode_tags                 = [
+  "app-kubernetes",
+  "clusterid-stable",
+  "type-workernode"
+]
