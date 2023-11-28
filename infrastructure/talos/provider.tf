87406fd1829a4a636e539184e32a6f9d4a8a6d6c
Shane Moo <obshane96@gmail.com>
2023-11-28 14:42:09 -0500

    Removed keys

--- infrastructure/talos/provider.tf
+++ infrastructure/talos/provider.tf
@@ -11,9 +11,13 @@ terraform {
       version = "0.3.4"
     }
     macaddress = {
-      source = "ivoronin/macaddress"
+      source  = "ivoronin/macaddress"
       version = "0.3.0"
-    } 
+    }
+    unifi = {
+      source  = "paultyng/unifi"
+      version = "0.41.0"
+    }
   }
 
   backend "s3" {
@@ -26,11 +30,21 @@ terraform {
 }
 
 provider "proxmox" {
-  endpoint = "https://192.168.1.64:8006/api2/json"
+  endpoint  = "https://192.168.1.64:8006/api2/json"
   api_token = "root@pam!tjs-test-2=fd03ad68-f6fd-4610-8dc2-4b6af00e7407"
   insecure  = true
   ssh {
     agent    = true
     username = "root"
   }
+}
+
+provider "unifi" {
+  # you may need to allow insecure TLS communications unless you have configured
+  # certificates for your controller
+  allow_insecure = true # optionally use UNIFI_INSECURE env var
+}
+
+data "unifi_network" "lan_network" {
+  name = "Default"
 }
