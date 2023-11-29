91c569e44b7c089bd39aca19537c77e82d1c0add
Shane Moo <obshane96@gmail.com>
2023-11-29 12:15:26 -0500

    Trying to turn this into a TF module

--- infrastructure/talos/talos_cluster.tf
+++ infrastructure/talos/talos_cluster.tf
@@ -1,7 +1,7 @@
 // locals {
-//   // controlplane_nodes = [for i in range(1, var.controlplane_num + 1) : "${var.controlplane_ip_prefix}${i}"]
-//   // worker_nodes       = [for i in range(1, var.workernode_num + 1) : "${var.workernode_ip_prefix}${i}"]
-//   controlplane_nodes = ["192.168.1.24"]
+//   // control_plane_nodes = [for i in range(1, var.control_plane_num + 1) : "${var.control_plane_ip_prefix}${i}"]
+//   // worker_nodes       = [for i in range(1, var.worker_node_num + 1) : "${var.worker_node_ip_prefix}${i}"]
+//   control_plane_nodes = ["192.168.1.24"]
 //   worker_nodes       = ["192.168.1.89"]
 // }
 
@@ -12,17 +12,17 @@
 // data "talos_client_configuration" "this" {
 //   cluster_name         = var.kubernetes_cluster_name
 //   client_configuration = talos_machine_secrets.this.client_configuration
-//   endpoints            = [for node in local.controlplane_nodes : node]
+//   endpoints            = [for node in local.control_plane_nodes : node]
 // }
 
 // resource "talos_machine_bootstrap" "this" {
-//   count = var.controlplane_num
+//   count = var.control_plane_num
 //   depends_on = [
-//     talos_machine_configuration_apply.controlplane
+//     talos_machine_configuration_apply.control_plane
 //   ]
 //   client_configuration = talos_machine_secrets.this.client_configuration
-//   endpoint             = local.controlplane_nodes[0]
-//   node                 = local.controlplane_nodes[0]
+//   endpoint             = local.control_plane_nodes[0]
+//   node                 = local.control_plane_nodes[0]
 // }
 
 
@@ -31,8 +31,8 @@
 //     talos_machine_bootstrap.this
 //   ]
 //   client_configuration = talos_machine_secrets.this.client_configuration
-//   endpoint             = local.controlplane_nodes[0]
-//   node                 = local.controlplane_nodes[0]
+//   endpoint             = local.control_plane_nodes[0]
+//   node                 = local.control_plane_nodes[0]
 // }
 
 // output "kubeconfig" {
