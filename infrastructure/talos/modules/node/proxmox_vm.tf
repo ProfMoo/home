3cfc3a2f6d3a54ee316556525490680d3c4e7a4d
Shane Moo <obshane96@gmail.com>
2023-11-29 13:37:29 -0500

    Going to delete some files

--- infrastructure/talos/modules/node/proxmox_vm.tf
+++ infrastructure/talos/modules/node/proxmox_vm.tf
@@ -1,37 +1,34 @@
 # NOTE: This creates the random mac addresses that we assign to the VMs
-resource "macaddress" "worker_node_mac_addresses" {
-  count = var.worker_node_num
-}
+resource "macaddress" "mac_addresses" {}
 
 resource "proxmox_virtual_environment_vm" "worker_node" {
   depends_on = [
-    macaddress.worker_node_mac_addresses
+    macaddress.mac_addresses
   ]
 
-  count = var.worker_node_num
-  vm_id = "${var.worker_node_vmid_prefix}${count.index + 1}"
+  vm_id = var.id
 
-  pool_id   = proxmox_virtual_environment_pool.proxmox_resource_pool.id
-  node_name = var.worker_node_node_name
+  pool_id   = var.proxmox_pool
+  node_name = var.proxmox_node_name
 
-  name        = "${var.worker_node_hostname_prefix}-${count.index + 1}"
-  description = "${var.worker_node_hostname_prefix}: ${count.index + 1}"
-  tags        = var.worker_node_tags
+  name        = var.proxmox_node_name
+  description = var.description
+  tags        = var.tags
 
   scsi_hardware = "virtio-scsi-single"
 
   cpu {
-    cores = var.worker_node_cpu_cores
+    cores = var.cpu_cores
     type  = "x86-64-v2-AES"
   }
 
   memory {
-    dedicated = var.worker_node_memory
+    dedicated = var.memory
   }
 
   network_device {
-    bridge      = var.worker_node_network_device
-    mac_address = macaddress.worker_node_mac_addresses[count.index].address
+    bridge      = var.network_device
+    mac_address = macaddress.mac_addresses[count.index].address
     firewall    = false
   }
 
@@ -39,16 +36,16 @@ resource "proxmox_virtual_environment_vm" "worker_node" {
 
   # NOTE: This provides Talos a location to store its state when it's configured via the API.
   disk {
-    datastore_id = var.worker_node_datastore
+    datastore_id = var.datastore
     file_format  = "raw"
     interface    = "scsi0"
-    size         = var.worker_node_disk_size
+    size         = var.disk_size
   }
 
   # NOTE: This loads the Talos onto disk via block storage.
   cdrom {
     enabled   = true
-    file_id   = proxmox_virtual_environment_file.talos_image.id
+    file_id   = var.initial_boot_iso
     interface = "ide2"
   }
 
