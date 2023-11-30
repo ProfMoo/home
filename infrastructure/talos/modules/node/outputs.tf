7f26b39639557351134f99c924e8ed490e938725
Shane Moo <obshane96@gmail.com>
2023-11-29 22:03:34 -0500

    Experimenting with different outputs from the modules

--- infrastructure/talos/modules/node/outputs.tf
+++ infrastructure/talos/modules/node/outputs.tf
@@ -2,6 +2,10 @@ data "unifi_user" "vm" {
   mac = macaddress.mac_addresses.address
 }
 
-output "vm_ipv4_address" {
-  value = data.unifi_user.vm.ip
+output "mac_address" {
+  value = macaddress.mac_addresses.address
+}
+
+output "ipv4_address" {
+  value = "192.168.1.29"
 }
