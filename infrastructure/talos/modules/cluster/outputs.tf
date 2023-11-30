d09bf19d211cc5e0c3186e605491afa6aaf5cbdc
Shane Moo <obshane96@gmail.com>
2023-11-30 00:26:38 -0500

    Got all the resources down, but missing some final touches in the configs

--- infrastructure/talos/modules/cluster/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "control_plane_nodes_ips" {
-  value = { for key, instance in module.control_plane_node : key => { "ipv4_address" : instance.ipv4_address, "mac_address" : instance.mac_address } }
-}
