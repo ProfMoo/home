5b9459e0740afa54021b888915f58c92b03a3643
Shane O'Brien <32044819+ProfMoo@users.noreply.github.com>
2024-05-15 20:13:06 -0400

    chore(deps): update terraform proxmox to 0.56.1

--- infrastructure/modules/cluster/providers.tf
+++ infrastructure/modules/cluster/providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.53.1"
+      version = "0.56.1"
     }
     macaddress = {
       source  = "ivoronin/macaddress"
