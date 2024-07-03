6ac1dd742fa180356a3bcc894cb489a41ddb28de
Shane Moo <obshane96@gmail.com>
2024-07-03 11:09:51 -0400

    Build for x86 arch, update tag

--- containers/betanin/push.sh
+++ containers/betanin/push.sh
@@ -5,8 +5,8 @@ IMAGE_NAME="profmoo/betanin"
 VERSION="latest"
 GHCR_IO="ghcr.io"
 
-# Build the Docker image
-docker build -t ${GHCR_IO}/${IMAGE_NAME}:${VERSION} .
+# Build the Docker image for x86_64 arch
+docker buildx build --platform linux/amd64 -t ${GHCR_IO}/${IMAGE_NAME}:${VERSION} .
 
 # Push the Docker image to GitHub Container Registry
 docker push ${GHCR_IO}/${IMAGE_NAME}:${VERSION}
