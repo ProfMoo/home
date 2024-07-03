bce420af42fb1bf48d2238a0fe76026d6beaaef5
Shane Moo <obshane96@gmail.com>
2024-07-03 15:36:07 -0400

    Update betanin script in QBT

--- kubernetes/homelab/apps/media/qbittorrent/app/scripts/betanin.sh
+++ kubernetes/homelab/apps/media/qbittorrent/app/scripts/betanin.sh
@@ -9,7 +9,7 @@ echo "[betanin]: Running curl against betanin..."
 
 # NOTE: For some reason I need to specify the full path for curl to work here
 # OK with adding an API Key here for now since it's only an API Key for a process running on local network (not exposed publicly)
-if ! /usr/bin/curl --request POST --data-urlencode "both=$PATH" --header "X-API-Key: f3d5a5a95fad096470ca411eb41161fb" http://192.168.1.78:9393/api/torrents; then
+if ! /usr/bin/curl --request POST --data-urlencode "both=$PATH" --header "X-API-Key: f3d5a5a95fad096470ca411eb41161fb" http://betanin.media.svc.cluster.local:9393/api/torrents; then
     echo "[betanin]: Adding new torrent '$PATH' to betanin queue failed."
 else
     echo "[betanin]: Adding new torrent '$PATH' to betanin queue succeeded."
