ea4bb09aaf5128c65bf79dd9c41d8d69eb4c34a9
Shane Moo <obshane96@gmail.com>
2024-09-29 11:45:50 -0400

    Add SOA record

--- aws/src/drmoo-io.tf
+++ aws/src/drmoo-io.tf
@@ -14,3 +14,14 @@ resource "aws_route53_record" "drmoo_io_ns" {
     "gloria.ns.cloudflare.com"
   ]
 }
+
+resource "aws_route53_record" "drmoo_io_soa" {
+  zone_id = aws_route53_zone.drmoo_io.zone_id
+  name    = "drmoo.io"
+  type    = "SOA"
+  ttl     = 900
+
+  records = [
+    "camilo.ns.cloudflare.com. admin.drmoo.io. 1 7200 900 1209600 86400"
+  ]
+}
